428636b2b5a77233ad4d4d19fde9654e28ae8001
atrox3d <atrox3d@gmail.com>
2022-11-25 06:50:55 +0100

    first commit

---
+++ update.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+PUSH=false
+AUTOCOMMIT=false
+
+function syntax()
+{
+	echo "ERROR  | missing/wrong param"
+	echo "SYNTAX | $0 [ -p -c -a ]"
+	echo "SYNTAX | -p push"
+	echo "SYNTAX | -c autocommit"
+	echo "SYNTAX | -a autocommit AND push"
+}
+
+
+if [ $# -gt 0 ]
+then
+	case "${1,,}" in
+		"-p")
+			PUSH=true
+			echo PUSH=true
+		;;
+		"-c")
+			AUTOCOMMIT=true
+			echo AUTOCOMMIT=true
+		;;
+		"-a")
+			AUTOCOMMIT=true
+			echo AUTOCOMMIT=true
+			PUSH=true
+			echo PUSH=true
+		;;
+		"-h")
+		;&
+		*)
+			syntax
+			exit
+		;;
+	esac
+else
+	syntax
+	exit
+fi
+
+
+dirs=(
+	black-hole-sun black-hole-sun-moises
+	man-in-the-box man-in-the-box-moises
+	cochise cochise-moises 
+	spoonman spoonman-moises 
+	killing-in-the-name-of-moises
+)
+for d in ${dirs[@]}
+do
+	echo "INFO | pulling $d..."
+	(cd $d; git pull)
+
+	if $AUTOCOMMIT
+	then
+		echo "INFO | auto committing $d..."
+		(
+			cd $d
+			git add .
+			git commit -am "autoupdate"
+		)
+	fi
+
+	if $PUSH 
+	then
+		echo "INFO | pushing $d..."
+		(cd $d; git push)
+	fi
+done
