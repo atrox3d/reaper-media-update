ebae996db4ad638b514b82b751b2bea836459951
atrox3d <atrox3d@gmail.com>
2025-01-24 08:46:47 +0100

    implement --log-level via enum and ctx

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import typer
 import logging
+from enum import Enum
 
 from proxy import (
     simplegit,
@@ -9,25 +10,43 @@ from proxy import (
     config
 )
 
+
 logger = logging.getLogger(__name__)
 app = typer.Typer()
 
 
+choices = list(
+    zip(
+        list(logging.getLevelNamesMapping()),
+        list(logging.getLevelNamesMapping()),
+    )
+)
+LogLevels = Enum('LogLevels', choices)
+
+
 @app.callback(invoke_without_command=True)
-def init(ctx:typer.Context):
+def init(ctx:typer.Context, log_level:LogLevels = 'INFO'):
     logging.basicConfig(
-        level=logging.DEBUG
+        level=log_level.value
     )
     
     cfg = config.JsonConfig().load()
     
     ctx.ensure_object(dict)
     ctx.obj['cfg'] = cfg
+    ctx.obj['log_level'] = log_level
 
 
 @app.command()
 def main(ctx:typer.Context):
-    print(f'{ctx.obj = }')
+    logger.info(f'{ctx.obj = }')
+    
+    # test --log-level
+    logger.debug('test')
+    logger.info('test')
+    logger.warning('test')
+    logger.error('test')
+    logger.fatal('test')
 
 
 if __name__ == "__main__":
