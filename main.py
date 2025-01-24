05f189b1cfc3f9fc74aacc5723e6ebca27d374e9
atrox3d <atrox3d@gmail.com>
2025-01-24 06:45:20 +0100

    testing typer

--- main.py
+++ main.py
@@ -4,18 +4,26 @@ import sys
 # sys.path.insert(0, 'python_argparse')
 # print(sys.path)
 # from python_argparse.main import main
+
 app = typer.Typer()
+subapp = typer.Typer(name='sub')
+app.add_typer(subapp)
 
+@app.callback(invoke_without_command=True)
+def callback(what:str='callback_what'):
+    print(f'callback {what = }')
+
+@subapp.callback(invoke_without_command=True)
+def subcallback(what:str='subcallback_what'):
+    print(f'subcallback {what = }')
 
 @app.command()
-def main(name:str):
-    print('hello ' + name)
+def main(name:str, capitalize:bool=False):
+    print(f'main {name = }, {capitalize = }')
 
-
-@app.command()
-def mein(name:str):
-    print('hello ' + name)
-
+@subapp.command('main')
+def submain(name:str):
+    print(f'sub {name = }')
 
 # typer.run(
     # main
